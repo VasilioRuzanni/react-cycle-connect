265c7da7f07f0f27db09ec6c2432516d459ef514
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-02-26 17:49:52 +0300

    refactor(interactions): Cleanup the interactions-related types, removing redundant internal types

--- src/interactions/types.ts
+++ src/interactions/types.ts
@@ -5,13 +5,6 @@ export type InteractFn = (
   predefinedValue?: any
 ) => (value?: any) => void;
 
-// TODO: Either back to it or remove
-// export type InteractionsProp<TInteractionEvents> = {
-//   [P in keyof TInteractionEvents]: (
-//     predefinedValue?: TInteractionEvents[P]
-//   ) => (eventValue?: TInteractionEvents[P]) => void
-// };
-
 export type InteractionsProp<TInteractionEvents> = {
   [P in keyof TInteractionEvents]: (eventValue?: TInteractionEvents[P]) => void
 };
@@ -26,28 +19,13 @@ export interface InteractionEvent<TType = any, TValue = any> {
   value?: TValue;
 }
 
-// export interface InteractionsSink<
-//   T = {
-//     [propName: string]: Stream<any>;
-//   }
-// > extends Stream<T> {}
-
-// TODO: Extend it to preserve the event types somehow, or make
-// easily overridable with explicitly passed type (i.e., as a
-// second generic param)
+// TODO: Extend it to preserve the original event types somehow
 export type InteractionsSink<TProps, TOverrides = {}> = Stream<
   Partial<{ [P in keyof TProps]: Stream<any> }> & TOverrides
 >;
 
-export interface InteractionsSourceInternal {
+export type InteractionsSource<TEvent = any> = {
   select<TEvent = any, TInteractionType extends string = string>(
     interactionSelector: string
   ): Stream<TEvent>;
-}
-
-export type DynamicInteractionStreams<TEvent> = {
-  [P in keyof TEvent]: Stream<TEvent[P]>
-};
-
-export type InteractionsSource<T = any> = InteractionsSourceInternal &
-  DynamicInteractionStreams<T>;
+} & { [P in keyof TEvent]: Stream<TEvent[P]> };
