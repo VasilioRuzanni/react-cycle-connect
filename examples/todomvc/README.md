7f559ad39eeb6ccdc900e64a3e8f59ef24d55028
Vasilio Ruzanni <vasilioruzanni@gmail.com>
2018-02-26 17:34:23 +0300

    feat(examples): Extend todomvc example with saving the state to a localStorage

--- examples/todomvc/README.md
+++ examples/todomvc/README.md
@@ -5,6 +5,8 @@ Built with:
 - [React](https://reactjs.org/) (with the help of [`create-react-app-typescript`](https://github.com/wmonk/create-react-app-typescript))
 - [Cycle.js](https://cycle.js.org/)
 - [`cycle-onionify`](https://github.com/staltz/cycle-onionify)
+- [`cycle-storageify`](https://github.com/maiermic/cycle-storageify)
+- [`@cycle/storage`](https://github.com/cyclejs/storage)
 - [`react-cycle-connect`](https://github.com/VasilioRuzanni/react-cycle-connect) (with `extra/onionify` package included).
 
 ## Start it up
--- examples/todomvc/src/RootCycle.tsx
+++ examples/todomvc/src/RootCycle.tsx
@@ -1,11 +1,24 @@
 import { ReactNode } from 'react';
 import { run } from '@cycle/run';
 import { makeHTTPDriver } from '@cycle/http';
+import storageDriver from '@cycle/storage';
 import onionify from 'cycle-onionify';
-import { cycleConnect } from 'react-cycle-connect';
+import storageify from 'cycle-storageify';
+import { cycleConnect, CycleMainFn } from 'react-cycle-connect';
 
-const drivers = { HTTP: makeHTTPDriver() };
-const wrappers = [onionify];
+const drivers = {
+  HTTP: makeHTTPDriver(),
+  storage: storageDriver
+};
+
+const wrappers = [
+  // TODO: The `CycleMainFn` is currently incompatible with Cycle.js'
+  // `Component` type but is being reconsidered in this regard,
+  // hence this hacky `any`
+  (mainFn: CycleMainFn) =>
+    storageify(mainFn as any, { key: 'react-cycle-connect-todomvc-state' }),
+  onionify
+];
 
 export interface Props {
   children: ReactNode;
